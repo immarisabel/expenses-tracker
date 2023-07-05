0e3efa7bdd6a329e882bc9588af2765cd6cb1f68
Marisabel <mail@marisabel.nl>
2023-07-05 17:40:16 +0200

    documenting categories and cleaning

--- src/main/java/nl/marisabel/backend/categories/service/CategoryService.java
+++ src/main/java/nl/marisabel/backend/categories/service/CategoryService.java
@@ -5,6 +5,7 @@ import nl.marisabel.backend.categories.entity.CategoryEntity;
 import nl.marisabel.backend.categories.repository.CategoryRepository;
 import nl.marisabel.backend.transactions.entity.TransactionEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,51 +13,45 @@ import java.util.List;
 
 
 @Service
-    public class CategoryService {
+public class CategoryService {
 
-        @Autowired
-        CategoryRepository categoryRepository;
+ @Autowired
+ CategoryRepository categoryRepository;
 
-        public List<CategoryEntity> getCategories(){
-            List<CategoryEntity> category = new ArrayList<>();
-            categoryRepository.findAll().forEach(category::add);
-            return category;
-        }
+    /**
+     * GET ALL CATEGORIES
+     * @return list of categories sorted in ASC alphabetically
+     */
+ public List<CategoryEntity> getCategories() {
+  List<CategoryEntity> category = new ArrayList<>();
+  categoryRepository.findAll(Sort.by(Sort.Direction.ASC, "category")).forEach(category::add);
+  return category;
+ }
 
-        public CategoryEntity getCategory(Long id){
-            return categoryRepository.findById(id).get();
-        }
+    /**
+     * GET SPECIFIC CATEGORY
+     * @param id
+     * @return single object of CategoryEntity
+     */
+ public CategoryEntity getCategory(Long id) {
+  return categoryRepository.findById(id).get();
+ }
 
-        public void saveOrUpdate(CategoryEntity category) {
-            categoryRepository.save(category);
-        }
+    /**
+     * CategoryEntity CRUD functions
+     */
+ public void saveOrUpdate(CategoryEntity category) {
+  categoryRepository.save(category);
+ }
 
-        public void deleteCategory(Long id) {
-            categoryRepository.deleteById(id);
-        }
+ public void deleteCategory(Long id) {
+  categoryRepository.deleteById(id);
+ }
 
+ private CategoryEntity createCategory(String category) {
+  CategoryEntity newCategory = new CategoryEntity();
+  newCategory.setCategory(category);
+  return categoryRepository.save(newCategory);
+ }
 
-    public CategoryEntity getOrCreateCategory(AutoCategoryEntity autoCategory) {
-        CategoryEntity existingCategory = categoryRepository.findByCategory(autoCategory.getCategory());
-
-        if (existingCategory == null) {
-            existingCategory = createCategory(autoCategory.getCategory());
-        }
-
-        return existingCategory;
-    }
-
-    private CategoryEntity createCategory(String category) {
-        CategoryEntity newCategory = new CategoryEntity();
-        newCategory.setCategory(category);
-        return categoryRepository.save(newCategory);
-    }
-
-    public boolean isTransactionMatch(TransactionEntity transaction, String query) {
-        String transactionEntity = transaction.getEntity().toLowerCase();
-        String transactionDescription = transaction.getDescription().toLowerCase();
-        String queryLower = query.toLowerCase();
-
-        return transactionEntity.contains(queryLower) || transactionDescription.contains(queryLower);
-    }
 }
